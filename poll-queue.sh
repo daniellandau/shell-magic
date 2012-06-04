e5cc3ad298767d44aafe5ea3c6d0b0849e36b44c
Daniel Landau <daniel.landau@iki.fi>
2012-06-04 15:15:41 +0300

    Fixed bug: '=' -> '==' in poll-queue.sh.

--- poll-queue.sh
+++ poll-queue.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-(date; qstat | awk '$5 = "Q" {print $0;}' | sed -n -e '1,/landau/p' | wc -l) | tee -a $WRKDIR/queue-stat
+(date; qstat | awk '$5 == "Q" {print $0;}' | sed -n -e '1,/landau/p' | wc -l) | tee -a $WRKDIR/queue-stat
 
