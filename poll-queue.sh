cb1eb39050b0422982539e351be72f8dc5fff64a
Daniel Landau <daniel.landau@iki.fi>
2012-06-07 16:06:48 +0300

    Fixed an off-by-one error. It showed one too many.

--- poll-queue.sh
+++ poll-queue.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
-(date; qstat | awk '$5 == "Q" {print $0;}' | sed -n -e '1,/landau/p' | wc -l) | tee -a $WRKDIR/queue-stat
-
+(date; echo $(qstat | awk '$5 == "Q" {print $0;}' | sed -n -e '1,/landau/p' | wc -l) - 1 | bc -lq) | tee -a $WRKDIR/queue-stat
