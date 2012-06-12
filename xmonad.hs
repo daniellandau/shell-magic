659740c8c47594e0e5262d18fb8dd6a2e9769f85
Daniel Landau <daniel.landau@iki.fi>
2012-06-12 17:07:42 +0300

    Changed layouts.

--- xmonad.hs
+++ xmonad.hs
@@ -1,28 +1,26 @@
 import XMonad
 import XMonad.Config.Gnome
 
---import XMonad.Actions.CycleWindows
 import XMonad.Actions.RotSlaves
 import XMonad.Actions.CycleWS
 
 import XMonad.Util.EZConfig
 
 import XMonad.Layout.Grid
-import XMonad.Layout.Circle
 import XMonad.Layout.TwoPane
-
+import XMonad.Hooks.ManageDocks -- avoidStruts for gnome panel
 
 --import System.IO
 
-myLayout = Full ||| Grid ||| TwoPane (3/100) (1/2)
+myLayout = avoidStruts (Mirror (Tall 1 (3/100) (2/3)) ||| TwoPane (3/100) (1/2) ||| Full)
 
 main = do
-  -- h <- openFile "/home/dlandau/.xmonad/out" WriteMode
-  -- hPutStrLn h $ show (layoutHook gnomeConfig)
-  -- hClose h
+--  h <- openFile "/home/dlandau/.xmonad/out" WriteMode
+--  hPutStrLn h $ show (layoutHook gnomeConfig)
+--  hClose h
   xmonad $ gnomeConfig {
          modMask = mod4Mask
---       , layoutHook = myLayout
+       , layoutHook = myLayout
        } `additionalKeysP`
        [ ("M-C-j", rotAllDown)
        , ("M-C-k", rotAllUp)
